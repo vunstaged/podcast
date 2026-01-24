450d37cab76976310d5a4c98553f323727298165
Iulian Cirlig <cirligiulian12312@gmail.com>
2026-01-24 18:20:08 +0200

    feat: commited refactor on 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,26 +1,26 @@
-import * as React from "react"
-import { Link, HeadFC, PageProps } from "gatsby"
+import * as React from 'react'
+import { Link, HeadFC, PageProps } from 'gatsby'
 
 const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  color: '#232129',
+  padding: '96px',
+  fontFamily: '-apple-system, Roboto, sans-serif, serif'
 }
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
-  maxWidth: 320,
+  maxWidth: 320
 }
 
 const paragraphStyles = {
-  marginBottom: 48,
+  marginBottom: 48
 }
 const codeStyles = {
-  color: "#8A6534",
+  color: '#8A6534',
   padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
+  backgroundColor: '#FFF4DB',
+  fontSize: '1.25rem',
+  borderRadius: 5git
 }
 
 const NotFoundPage: React.FC<PageProps> = () => {
@@ -30,7 +30,7 @@ const NotFoundPage: React.FC<PageProps> = () => {
       <p style={paragraphStyles}>
         Sorry 😔, we couldn’t find what you were looking for.
         <br />
-        {process.env.NODE_ENV === "development" ? (
+        {process.env.NODE_ENV === 'development' ? (
           <>
             <br />
             Try creating a page in <code style={codeStyles}>src/pages/</code>.
@@ -38,7 +38,7 @@ const NotFoundPage: React.FC<PageProps> = () => {
           </>
         ) : null}
         <br />
-        <Link to="/">Go home</Link>.
+        <Link to='/'>Go home</Link>.
       </p>
     </main>
   )
